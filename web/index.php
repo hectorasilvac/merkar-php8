90bc2fc44e634efdb74b9c44b9925f877f5428e2
silvahector <hascardenas@gmail.com>
2021-02-08 19:29:12 -0500

    Se corrige estructura MVC

--- web/index.php
+++ web/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 try {
@@ -18,6 +17,3 @@ try {
   in ' . $e->getFile() . ':' . $e->getLine();
   include __DIR__ . '/../templates/layout.html.php';
 }
-=======
-<?php echo 'Hola' ?>
->>>>>>> cd18ecb90c96f3e9c0741400651d4a715a5ef982
